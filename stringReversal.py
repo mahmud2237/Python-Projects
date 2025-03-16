243b5bc1fc9ba8a32dc8278d45a9cf54e88eec54
mahmud2237 <smahmud2237@gmail.com>
2025-03-16 22:27:43 +0600

    String Reversal

---
+++ stringReversal.py
@@ -0,0 +1,14 @@
+# Project: A program that reverses the string provided by the user
+
+# String Reversal welcome message 
+print('Welcome to the String reversal Program!')
+
+# Get user Input
+user_str = input('Enter a string to Reverse: ')
+
+# Reverse the String
+reversed_str = user_str[::-1]
+
+# Now display the result
+print('The Reversal String is: ', reversed_str)
+
